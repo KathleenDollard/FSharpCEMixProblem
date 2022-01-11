7cadc76a6cdc5babb07f1cab6a74da8c5fb650f0
Kathleen Dollard <kdollard@microsoft.com>
2022-01-11 07:39:27 -0800

    Updated with Don's CE implementation, fixed typo with two Z6's. Shows error

--- FSharpCEMixProblem/Program.fs
+++ FSharpCEMixProblem/Program.fs
@@ -1,69 +1,144 @@
-﻿namespace X
+﻿module CEs =
 
-open System
+    type M<'T, 'Vars> =
+        { Name: string option
+          Members: 'T list
+          Variables: 'Vars }
 
-type M<'T> =
-    { Name: string
-      MemberList: 'T list }
+    type M<'T> = M<'T, unit>
 
-
-type CE() =
+    type CE() =
             
-    member _.Zero() : M<'T> = { Name =""; MemberList = [] }
-    member _.Combine (item1: M<'T>, item2: M<'T>) : M<'T> =
-        let newName = if String.IsNullOrWhiteSpace(item2.Name) then item1.Name else item2.Name
-        { Name = newName; MemberList = List.append item1.MemberList item2.MemberList }
-    member _.Delay(f) : M<'T> = f()
-    member this.For(methods, f) :M<'T> = 
-        let methodList = Seq.toList methods
-        match methodList with 
-        | [] -> this.Zero()
-        | [x] -> f(x)
-        | head::tail ->
-            let mutable headResult = f(head)
-            for x in tail do 
-                headResult <- this.Combine(headResult, f(x))
-            headResult
+        member _.Zero() : M<'T> =
+            { Name = None
+              Members = [] 
+              Variables = () }
 
-    member this.Yield (()) : M<'T> = 
-        this.Zero() 
-    member this.Yield (item: 'T) : M<'T> = 
-        { this.Zero() with MemberList = [ item ]}
+        member _.Combine (model1: M<'T>, model2: M<'T>) : M<'T> =
+            let newName = 
+                match model2.Name with
+                | None -> model1.Name
+                | res -> res
+            { Name = newName
+              Members = List.append model1.Members model2.Members 
+              Variables = () }
 
-    [<CustomOperation("Name")>]
-    member _.setName (model: M<'T>, name: string) : M<'T>  =
-        { model with Name = name }
-    [<CustomOperation("Member")>]
-    member _.addMembers (model: M<'T>, item)  =
-        { model with MemberList = List.append model.MemberList [ item ] }
-    [<CustomOperation("Members", MaintainsVariableSpace = true)>]
-    member _.addMembers (model: M<'T>, [<ParamArray>] items: 'T[]) : M<'T>  =
-        { model with MemberList = List.append model.MemberList (List.ofArray items) }
+        member _.Delay(f) : M<'T, 'Vars> = f()
 
-module Test =
-    let x =
-        CE() { Name "Fred" }
+        member _.Run(model: M<'T, 'Vars>) : M<'T> =
+            { Name = model.Name
+              Members = model.Members
+              Variables = () }
 
-    let y = 
-        CE() { 42 }
+        member this.For(methods, f) :M<'T> = 
+            let methodList = Seq.toList methods
+            match methodList with 
+            | [] -> this.Zero()
+            | [x] -> f(x)
+            | head::tail ->
+                let mutable headResult = f(head)
+                for x in tail do 
+                    headResult <- this.Combine(headResult, f(x))
+                headResult
 
-    let z =
-        CE() { Member 42 }
+        member _.Yield (item: 'T) : M<'T> = 
+            { Name = None
+              Members = [ item ]
+              Variables = () }
 
-    let z2 =
-        CE() {
-            Members 
-                42 
-                43
-        }
+        // Only for packing/unpacking the implicit variable space
+        member _.Bind (model1: M<'T, 'Vars>, f: ('Vars -> M<'T>)) : M<'T>  =
+            let model2 = f model1.Variables
+            let newName = 
+                match model2.Name with
+                | None -> model1.Name
+                | res -> res
+            { Name = newName
+              Members = model1.Members @ model2.Members
+              Variables = model2.Variables }
 
-    let failure = 
-        CE() {
-            Name "Fred"
-            42
-        }
+        // Only for packing/unpacking the implicit variable space
+        member _.Return (varspace: 'Vars) : M<'T, 'Vars> = 
+            { Name = None
+              Members = [ ]
+              Variables = varspace }
 
-    let empty = 
-        CE() {}
+        [<CustomOperation("Name", MaintainsVariableSpaceUsingBind = true)>]
+        member _.setName (model: M<'T, 'Vars>, [<ProjectionParameter>] name: ('Vars -> string)) : M<'T, 'Vars>  =
+            { model with Name = Some (name model.Variables) }
 
-    
+        [<CustomOperation("Member", MaintainsVariableSpaceUsingBind = true)>]
+        member _.addMember (model: M<'T, 'Vars>, [<ProjectionParameter>] item: ('Vars -> 'T))  : M<'T, 'Vars>  =
+            { model with Members = List.append model.Members [ item model.Variables ] }
+
+        // Note, using ParamArray doesn't work in conjunction with ProjectionParameter
+        [<CustomOperation("Members", MaintainsVariableSpaceUsingBind = true)>]
+        member _.addMembers (model: M<'T, 'Vars>, [<ProjectionParameter>] items: ('Vars -> 'T list)) : M<'T, 'Vars>  =
+            { model with Members = List.append model.Members (items model.Variables) }
+
+    let ce = CE()
+    module Test =
+        let x : M<double> =
+            ce { Name "Fred" }
+
+        let y = 
+            ce { 42 }
+
+        let z1 =
+            ce { Member 42 }
+
+        let z2 = 
+            ce {
+                Members [ 41; 42 ]
+            }
+        let z3 = 
+            ce {
+                Name "Fred"
+                42
+            }
+
+        let z4 = 
+            ce {
+                Member 41
+                Member 42
+            }
+
+        let z5 = 
+            ce {
+                Name "a"
+                Name "b"
+            }
+
+        let z6 : M<double> = 
+            ce {
+                let x = 1
+                Name "a"
+                Member 4.0
+            }
+
+        let z7 : M<double> = 
+            ce {
+                let x = "a"
+                Name (x + "b")
+                Member 4.0
+            }
+
+        let z8 : M<double> = 
+            ce {
+                let x1 = 1.0
+                let y2 = 2.0
+                Member (x1 + 3.0)
+                Member (y2 + 4.0)
+            }
+
+        let z9 =
+            ce {
+                let x = 1.0
+                Members [
+                    42.3 
+                    43.1 + x
+                ]
+            }
+
+        //let empty = 
+        //    ce { }
