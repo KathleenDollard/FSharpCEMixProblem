39ef6ef003cf0c72d8dc38637807e2ddf8dc3b8f
Kathleen Dollard <kdollard@microsoft.com>
2022-01-11 15:57:16 -0800

    WIP demo code

---
+++ Playground.FSharp/Program.fs
@@ -0,0 +1,19 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+let x = [
+    "Hello"
+    "world"
+    for i in [1..10] do
+        i.ToString()
+        $"{i}"
+    "The"
+    "End" ]
+
+
+//type Room =
+//    | Closet of RodLength: int
+//    | Bedroom of Width: int * Length: int
+//    | Kitchen of SinkDepth: int * Dishwasher: bool * Stove: Stove option
+//    | Unknown
+
+
+printfn "Hello from F#"
