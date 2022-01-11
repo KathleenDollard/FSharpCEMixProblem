3da53a003419f10e6c73fe8b8b810515db873a16
Kathleen Dollard <kdollard@microsoft.com>
2022-01-11 07:15:39 -0800

    C# struct initialization issues

---
+++ Playground.CSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using Playground.CSharp;
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        // removed because that would give an error in the struct in 17.1
+        //var x = new S0();
+        //Console.WriteLine(x.X.ToString());
+
+
+        var x3 = new S1();
+        Console.WriteLine(x3.X.ToString());
+
+        // removed because that would give an error here in 17.1
+        //var x4 = new S2();
+        //Console.WriteLine(x4.X.ToString());
+
+        var x5 = new S3();
+        Console.WriteLine(x5.X.ToString());
+
+        var x6 = new S3("Hello");
+        Console.WriteLine(x5.X.ToString());
+
+    }
+}
---
+++ Playground.CSharp/StructOne.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground.CSharp
+{
+
+
+    struct S0
+    {
+        public int X { get; set; } = 5;
+    }
+
+    struct S1
+    {
+        public int X { get; set; } = 5;
+        public S1() { }
+    }
+
+    struct S2
+    {
+        public int X { get; set; } = 5;
+        public S2(string message) { }
+    }
+
+    struct S3
+    {
+        public int X { get; set; } = 5;
+        public S3() { }
+
+        public S3(string message) { }
+
+    }
+
+    struct S4
+    {
+        //public S3() 
+        //{
+        //    // WHat is Message?
+        //}
+        public S4(string msg) => Message = msg;
+
+        public string Message { get; init; }
+    }
+
+    record struct RS0(string first, string last, string? optional = null)
+    {
+        public string? OptionalMessage { get; } = optional;
+    }
+    record struct RS1(string first, string last)
+    {
+        public string first { get; set; } = last;
+
+        public RS1() : this("Fred", "George")
+        {
+            // What is "first" and "last"?
+        }
+    }
+
+}
