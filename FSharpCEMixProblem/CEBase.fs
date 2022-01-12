2e60e4fef2e9f36691ed2fdbcfe7b52d1cd07b48
Kathleen Dollard <kdollard@microsoft.com>
2022-01-12 06:55:39 -0800

    Reorganized CEBase

---
+++ FSharpCEMixProblem/CEBase.fs
@@ -0,0 +1,111 @@
+﻿namespace CEBase 
+
+    type membership<'TItem> = 
+        { Name: string option
+          IsMember: bool option
+          IsMember2: bool  // confirming that shape of the container is not restricted, just need clear default the CE understands
+          Members: 'TItem list }
+
+    type M<'T, 'Vars> =
+        { Model: 'T
+          Variables: 'Vars }
+
+    type M<'T> = M<'T, unit>
+
+    type DslBase() =
+        let empty() = 
+             { Name = None
+               IsMember = None
+               IsMember2 = false
+               Members = [] }
+
+        let combine model1 model2 =
+            let newName = 
+                match model2.Name with
+                | None -> model1.Name
+                | res -> res
+            let newIsMember =
+                match model2.IsMember with 
+                | None -> model1.IsMember
+                | res -> res
+            let newIsMember2 =
+                match model2.IsMember2 with 
+                | true -> true
+                | res -> res
+            { Name = newName
+              IsMember = newIsMember
+              IsMember2 = newIsMember2
+              Members =  model1.Members @ model2.Members }
+
+        member _.Zero() : M<'T, unit> = 
+            { Model = empty() 
+              Variables = () }
+
+        member _.Combine (model1: M<'T, unit>, model2: M<'T, unit>) : M<'T, unit> =
+            { Model = combine model1.Model model2.Model
+              Variables = () }
+
+        member _.Delay(f) : M<'T, 'Vars> = f()
+
+        member _.Run(model: M<'T, 'Vars>) : M<'T, unit> =
+            { Model = combine model.Model (empty())
+              Variables = () }
+
+        member this.For(methods, f) :M<'T, unit> = 
+            let methodList = Seq.toList methods
+            match methodList with 
+            | [] -> this.Zero()
+            | [x] -> f(x)
+            | head::tail ->
+                let mutable headResult = f(head)
+                for x in tail do 
+                    headResult <- this.Combine(headResult, f(x))
+                headResult
+
+        member _.Yield (item: 'TItem) : M<'T, unit> = 
+            { Model = { empty() with Members = [ item ] }
+              Variables = () }
+
+        // Only for packing/unpacking the implicit variable space
+        member _.Bind (model1: M<'T, 'Vars>, f: ('Vars -> M<'T, unit>)) : M<'T, unit>  =
+            let model2 = f model1.Variables
+            let combined = combine model1.Model model2.Model
+            { Model = combine model1.Model model2.Model
+              Variables = model2.Variables }
+
+        // Only for packing/unpacking the implicit variable space
+        member _.Return (varspace: 'Vars) : M<'T, 'Vars> = 
+            { Model = empty() 
+              Variables = varspace }
+
+        member _.SetModel (model: M<'T, 'Vars>) (newModel: 'T) =
+            { Model = newModel
+              Variables = model.Variables }            
+
+    type CE() =
+        inherit DslBase()
+
+        [<CustomOperation("Name", MaintainsVariableSpaceUsingBind = true)>]
+        member this.setName (model: M<'T, 'Vars>, [<ProjectionParameter>] name: ('Vars -> string)) : M<'T, 'Vars>  =
+            this.SetModel model { model.Model with Name = Some (name model.Variables) }
+
+        [<CustomOperation("IsMember", MaintainsVariableSpaceUsingBind = true)>]
+        member this.setIsMember (model: M<'T, 'Vars>, [<ProjectionParameter>] isMember: ('Vars -> bool)) : M<'T, 'Vars>  =
+            this.SetModel model { model.Model with IsMember = Some (isMember model.Variables) }
+
+        // We can skip 
+        [<CustomOperation("IsMember2", MaintainsVariableSpaceUsingBind = true)>]
+        member this.setIsMember2 (model: M<'T, 'Vars>, [<ProjectionParameter>] isMember: ('Vars -> bool)) : M<'T, 'Vars>  =
+            let newIsMember2 = isMember model.Variables
+            this.SetModel model { model.Model with IsMember2 = newIsMember2 }
+
+        [<CustomOperation("Member", MaintainsVariableSpaceUsingBind = true)>]
+        member this.addMember (model: M<'T, 'Vars>, [<ProjectionParameter>] item: ('Vars -> 'TItem))  : M<'T, 'Vars>  =
+            this.SetModel model { model.Model with Members = List.append model.Model.Members [ item model.Variables ] }
+
+        // Note, using ParamArray doesn't work in conjunction with ProjectionParameter
+        [<CustomOperation("Members", MaintainsVariableSpaceUsingBind = true)>]
+        member this.addMembers (model: M<'T, 'Vars>, [<ProjectionParameter>] items: ('Vars -> 'TItem list)) : M<'T, 'Vars>  =
+            this.SetModel model { model.Model with Members = List.append model.Model.Members (items model.Variables) }
+
+
--- FSharpCEMixProblem/Program.fs
+++ FSharpCEMixProblem/Program.fs
@@ -1,131 +1,31 @@
-﻿module CEs =
+﻿open CEBase
 
-    type M<'T, 'Vars> =
-        { Name: string option
-          IsMember: bool option
-          IsMember2: bool  // confirming that shape of the container is not restricted, just need clear default the CE understands
-          Members: 'T list
-          Variables: 'Vars }
 
-    type M<'T> = M<'T, unit>
-
-    type CE() =
-            
-        member _.Zero() : M<'T> =
-            { Name = None
-              IsMember = None
-              IsMember2 = false
-              Members = [] 
-              Variables = () }
-
-        member _.Combine (model1: M<'T>, model2: M<'T>) : M<'T> =
-            let newName = 
-                match model2.Name with
-                | None -> model1.Name
-                | res -> res
-            let newIsMember =
-                match model2.IsMember with 
-                | None -> model1.IsMember
-                | res -> res
-            let newIsMember2 =
-                match model2.IsMember2 with 
-                | true -> true
-                | res -> res
-            { Name = newName
-              IsMember = newIsMember
-              IsMember2 = newIsMember2
-              Members = List.append model1.Members model2.Members 
-              Variables = () }
-
-        member _.Delay(f) : M<'T, 'Vars> = f()
-
-        member _.Run(model: M<'T, 'Vars>) : M<'T> =
-            { Name = model.Name
-              IsMember = model.IsMember
-              IsMember2 = model.IsMember2
-              Members = model.Members
-              Variables = () }
-
-        member this.For(methods, f) :M<'T> = 
-            let methodList = Seq.toList methods
-            match methodList with 
-            | [] -> this.Zero()
-            | [x] -> f(x)
-            | head::tail ->
-                let mutable headResult = f(head)
-                for x in tail do 
-                    headResult <- this.Combine(headResult, f(x))
-                headResult
-
-        member _.Yield (item: 'T) : M<'T> = 
-            { Name = None
-              IsMember = None
-              IsMember2 = false
-              Members = [ item ]
-              Variables = () }
-
-        // Only for packing/unpacking the implicit variable space
-        member _.Bind (model1: M<'T, 'Vars>, f: ('Vars -> M<'T>)) : M<'T>  =
-            let model2 = f model1.Variables
-            let newName = 
-                match model2.Name with
-                | None -> model1.Name
-                | res -> res
-            let newIsMember =
-                match model2.IsMember with 
-                | None -> model1.IsMember
-                | res -> res
-            let newIsMember2 =
-                match model2.IsMember2 with 
-                | true -> true
-                | res -> res
-            { Name = newName
-              IsMember = newIsMember
-              IsMember2 = newIsMember2
-              Members = model1.Members @ model2.Members
-              Variables = model2.Variables }
-
-        // Only for packing/unpacking the implicit variable space
-        member _.Return (varspace: 'Vars) : M<'T, 'Vars> = 
-            { Name = None
-              IsMember = None
-              IsMember2 = false
-              Members = [ ]
-              Variables = varspace }
-
-        [<CustomOperation("Name", MaintainsVariableSpaceUsingBind = true)>]
-        member _.setName (model: M<'T, 'Vars>, [<ProjectionParameter>] name: ('Vars -> string)) : M<'T, 'Vars>  =
-            { model with Name = Some (name model.Variables) }
-
-        [<CustomOperation("IsMember", MaintainsVariableSpaceUsingBind = true)>]
-        member _.setIsMember (model: M<'T, 'Vars>, [<ProjectionParameter>] isMember: ('Vars -> bool)) : M<'T, 'Vars>  =
-            { model with IsMember = Some (isMember model.Variables) }
-
-        // We can skip 
-        [<CustomOperation("IsMember2", MaintainsVariableSpaceUsingBind = true)>]
-        member _.setIsMember2 (model: M<'T, 'Vars>, [<ProjectionParameter>] isMember: ('Vars -> bool)) : M<'T, 'Vars>  =
-            { model with IsMember2 = isMember model.Variables }
-
-        [<CustomOperation("Member", MaintainsVariableSpaceUsingBind = true)>]
-        member _.addMember (model: M<'T, 'Vars>, [<ProjectionParameter>] item: ('Vars -> 'T))  : M<'T, 'Vars>  =
-            { model with Members = List.append model.Members [ item model.Variables ] }
-
-        // Note, using ParamArray doesn't work in conjunction with ProjectionParameter
-        [<CustomOperation("Members", MaintainsVariableSpaceUsingBind = true)>]
-        member _.addMembers (model: M<'T, 'Vars>, [<ProjectionParameter>] items: ('Vars -> 'T list)) : M<'T, 'Vars>  =
-            { model with Members = List.append model.Members (items model.Variables) }
+module CEs =
 
     let ce = CE()
     module Test =
-        let x : M<double> =
+        let x : M<membership<double>> =
             ce { Name "Fred" }
 
-        let x2 : M<double> =
+        // IsMember2 is not working in the following
+        let x2 : M<membership<double>> =
             ce {
                 Name "Fred" 
                 IsMember true
                 IsMember true
                 IsMember2 true // Note, I can call this twice without compiler error, but not Name in z5
+                IsMember2 true
+                }
+
+        let x3 : M<membership<double>> =
+            ce {
+                Name "Fred" 
+                IsMember2 true
+                }
+
+        let x4 : M<membership<double>> =
+            ce {
                 IsMember2 true
                 }
 
@@ -143,6 +43,7 @@
             ce {
                 Name "Fred"
                 42
+                43
             }
 
         let z4 = 
@@ -151,28 +52,28 @@
                 Member 42
             }
 
-        let z5 : M<double> = 
+        let z5 : M<membership<double>> = 
             ce {
                 Name "a"
                 Name "b"
                 //42 // removing this line results in compiler error
             }
 
-        let z6 : M<double> = 
+        let z6 : M<membership<double>> = 
             ce {
                 let x = 1
                 Name "a"
                 Member 4.0
             }
 
-        let z7 : M<double> = 
+        let z7 : M<membership<double>> = 
             ce {
                 let x = "a"
                 Name (x + "b")
                 Member 4.0
             }
 
-        let z8 : M<double> = 
+        let z8 : M<membership<double>> = 
             ce {
                 let x1 = 1.0
                 let y2 = 2.0
